02e033293aafe1b4621617e0d93ca7f7cafdfe74
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-06-21 19:00:33 +0900

    Add WIP Component implementation

--- include/brica/component.hpp
+++ include/brica/component.hpp
@@ -1,4 +1,55 @@
 #ifndef __BRICA_KERNEL_COMPONENT_HPP__
 #define __BRICA_KERNEL_COMPONENT_HPP__
 
+#include "brica/assocvec.hpp"
+#include "brica/buffer.hpp"
+#include "brica/functor.hpp"
+
+#include <string>
+#include <utility>
+
+namespace brica {
+
+class Component {
+  class Channel {
+   public:
+    virtual void send(Buffer& value) { buffer = value; }
+    virtual const Buffer& recv() const { return buffer; }
+
+   private:
+    Buffer buffer;
+  };
+
+  using Channels = AssocVec<std::string, std::shared_ptr<Channel>>;
+
+ public:
+  void make_in_channel(std::string name) {
+    inputs.emplace(name);
+    in_channels.emplace(name);
+  }
+
+  void collect() {
+    for (std::size_t i = 0; i < inputs.size(); ++i) {
+      inputs[i] = in_channels[i]->recv();
+    }
+  }
+
+  void execute() { functor(inputs, outputs); }
+
+  void expose() {
+    for (std::size_t i = 0; i < outputs.size(); ++i) {
+      out_channels[i]->send(outputs[i]);
+    }
+  }
+
+ private:
+  Functor functor;
+  Dict inputs;
+  Dict outputs;
+  Channels in_channels;
+  Channels out_channels;
+};
+
+}  // namespace brica
+
 #endif  // __BRICA_KERNEL_COMPONENT_HPP__
