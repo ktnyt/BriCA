c69499b81a5a9a114e20003b458d4922a71e36c5
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-06-21 15:35:08 +0900

    Add Functor implementation

--- include/brica/functor.hpp
+++ include/brica/functor.hpp
@@ -1,4 +1,16 @@
 #ifndef __BRICA_KERNEL_FUNCTOR_HPP__
 #define __BRICA_KERNEL_FUNCTOR_HPP__
 
+#include <functional>
+
+#include "brica/assocvec.hpp"
+#include "brica/buffer.hpp"
+
+namespace brica {
+
+using Dict = AssocVec<std::string, Buffer>;
+using Functor = std::function<void(Dict, Dict)>;
+
+}  // namespace brica
+
 #endif  // __BRICA_KERNEL_FUNCTOR_HPP__
